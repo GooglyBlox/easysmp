96a1f61f8a4b261e99e88124f6d854ebecdb1f3a
GooglyBlox <cemcimyildiz@gmail.com>
2024-06-18 22:04:23 -0700

    fix: optimize images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,7 @@ export default function Home() {
             fill
             objectFit="cover"
             quality={100}
+            sizes="100vw"
             priority={index === 0}
             className={`transition-opacity duration-1000 ${
               index === currentImage ? "opacity-100" : "opacity-0"
